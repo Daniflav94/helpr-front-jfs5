7575fa9a1929896bffd987f86755d4be4e3e4184
Mohotrisk <diegosocial20@hotmail.com>
2022-12-12 09:34:58 -0300

    Criação de formulário para novo cargo

--- src/app/views/cargos/cargos-routing.module.ts
+++ src/app/views/cargos/cargos-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CargosComponent } from './cargos/cargos.component';
+import { NewCargoComponent } from './new-cargo/new-cargo.component';
 
-const routes: Routes = [{ path: '', component: CargosComponent }];
+const routes: Routes = [{ 
+  path: '', component: CargosComponent
+ },
+{
+  path: 'new', //qual vai chamar a new cargo
+  component: NewCargoComponent // rota que chama o new cargo 
+}
+ ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
